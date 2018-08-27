ce53009271d8c36ef4bb25178fb1de048b699c3a
MrLittlewell <40912379+MrLittlewell@users.noreply.github.com>
2018-08-27 22:01:09 +0300

    upd 27.08.2018

---
+++ js/js.js
@@ -0,0 +1,13 @@
+$(function(){
+	$('.slider').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  autoplay: true,
+	  autoplaySpeed: 2500,
+	  cssEase: 'linear',
+	  appendArrows: '.arrow'
+	  
+	});	
+});
