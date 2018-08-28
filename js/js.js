f1eb81dbc7b3cd427d29bbff1fe274019ecc6338
MrLittlewell <40912379+MrLittlewell@users.noreply.github.com>
2018-08-28 15:44:21 +0300

    upd 28.08.18

--- js/js.js
+++ js/js.js
@@ -7,7 +7,50 @@ $(function(){
 	  autoplay: true,
 	  autoplaySpeed: 2500,
 	  cssEase: 'linear',
-	  appendArrows: '.arrow'
-	  
+	  arrows: true,
+	  draggable: true,
+	  	  
 	});	
 });
+jQuery(document).ready(function($) {
+	var						 
+		$window = $(window), 						 
+		$target = $("#nav"),						 
+		$h = 180; // Задаю координаты верха на котором надо фиксировать						 
+		$window.on('scroll', function() {									
+	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;									
+		if (scrollTop > $h) {						 
+			$target.addClass("move-header");						 
+				} else {     
+					$target.removeClass("move-header");
+					}
+					});	
+});
+jQuery(document).ready(function($) {
+	var						 
+		$window = $(window), 						 
+		$target = $("#logo-move"),						 
+		$h = 180; // Задаю координаты верха на котором надо фиксировать						 
+		$window.on('scroll', function() {									
+	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;									
+		if (scrollTop > $h) {						 
+			$target.addClass("logo-size");						 
+				} else {     
+					$target.removeClass("logo-size");
+					}
+					});				
+});	
+jQuery(document).ready(function($) {
+	var						 
+		$window = $(window), 						 
+		$target = $("#nav-ancor"),						 
+		$h = 180; // Задаю координаты верха на котором надо фиксировать						 
+		$window.on('scroll', function() {									
+	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;									
+		if (scrollTop > $h) {						 
+			$target.addClass("ancor-menu-position");						 
+				} else {     
+					$target.removeClass("ancor-menu-position");
+					}
+					});				
+});											 
